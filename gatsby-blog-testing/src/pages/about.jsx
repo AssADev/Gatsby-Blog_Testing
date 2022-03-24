7d5d8caa25a49c360bb8ce012643ad137ec36e3a
AssADev <gravelteo.pro@gmail.com>
2022-03-24 14:55:19 +0100

    Gatsby setup

---
+++ gatsby-blog-testing/src/pages/about.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore sed
+        corrupti numquam, pariatur amet, excepturi quo iusto totam nihil non hic
+        delectus quas sit. Tempora?
+      </p>
+    </div>
+  )
+}
+
+export default About
